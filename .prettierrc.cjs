8624b6df8020e6ddb4c591e0248120144aa5740e
LIUZHAO <liuzhaomax@163.com>
2025-06-14 15:47:39 +0800

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: false,          // 不加分号
+  singleQuote: true,    // 使用单引号
+  tabWidth: 2,          // 缩进 2 空格
+  trailingComma: 'all', // 所有可能的地方加逗号
+  printWidth: 100,      // 每行最多 100 字符
+  endOfLine: 'lf',      // 换行符用 LF（Unix 风格）
+  bracketSpacing: true,     // 大括号内空格
+  arrowParens: 'always',    // 箭头函数强制加括号
+  jsxSingleQuote: false,    // jsx不使用单引号
+  quoteProps: 'as-needed',  // 对象属性名仅在需要时加引号
+  proseWrap: 'always',      // Markdown 文本自动换行
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# nankai-clock-tower-mp
-南开钟楼 - 血染钟楼无说书人辅助器 - 小程序
+小程序
+
+? 请输入项目名称！ nct
+? 请输入项目介绍 南开钟楼    
+? 请选择框架 React                 
+? 是否需要使用 TypeScript ？ Yes
+? 是否需要编译为 ES5 ？ No                                
+? 请选择 CSS 预处理器（Sass/Less/Stylus） Sass
+? 请选择包管理工具 npm           
+? 请选择编译工具 Vite          
+? 请选择模板源 Github（最新）  
+? 请选择模板 默认模板
+
+TODO：
++ lint
++ lint fix auto format
++ router
++ navi
++ UI
++ 请求
++ 部署
