7e1ad605684006a3d6daaa15e979238d06d1ee42
LIUZHAO <liuzhaomax@163.com>
2025-06-14 18:22:39 +0800

    init

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { useLaunch } from '@tarojs/taro'
 
 import './app.scss'
 
-function App({ children }: PropsWithChildren<any>) {
+function App({ children }: PropsWithChildren<object>) {
   useLaunch(() => {
     console.log('App launched.')
   })
@@ -11,7 +11,5 @@ function App({ children }: PropsWithChildren<any>) {
   // children 是将要会渲染的页面
   return children
 }
-  
-
 
 export default App
