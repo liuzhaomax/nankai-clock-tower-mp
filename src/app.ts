647c4069a362e3d23a15660c1cf382b909a911e1
LIUZHAO <liuzhaomax@163.com>
2025-06-18 23:36:53 +0800

    init

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
-import { useEffect } from 'react'
-import { useDidShow, useDidHide } from '@tarojs/taro'
+// import { useEffect } from 'react'
+// import { useDidShow, useDidHide } from '@tarojs/taro'
 // 全局样式
 import './app.scss'
 
-function App(props) {
-  // 可以使用所有的 React Hooks
-  useEffect(() => {})
+interface AppProps {
+  children?: React.ReactNode
+}
 
-  // 对应 onShow
-  useDidShow(() => {})
-
-  // 对应 onHide
-  useDidHide(() => {})
+const App = (props: AppProps) => {
+  // // 可以使用所有的 React Hooks
+  // useEffect(() => {})
+  //
+  // // 对应 onShow
+  // useDidShow(() => {})
+  //
+  // // 对应 onHide
+  // useDidHide(() => {})
 
   return props.children
 }
