4eeb489342acd42919d39c5afb15ae461e49134a
LIUZHAO <liuzhaomax@163.com>
2025-07-09 00:27:48 +0800

    login

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,9 +3,11 @@ const ENV = process.env.NODE_ENV || 'development'
 export const getRoutes = () => {
   const devRoutes = {
     BASE_URL: `https://blood.liuzhaomax.cn`,
+    LOGIN: '/login',
   }
   const prodRoutes = {
     BASE_URL: `https://blood.liuzhaomax.cn`,
+    LOGIN: '/login',
   }
   switch (ENV) {
     case 'development':
--- src/pages/home/hooks/auth.ts
+++ src/pages/home/hooks/auth.ts
@@ -4,22 +4,31 @@ import { getRoutes } from '@/config/api'
 
 const ROUTES = getRoutes()
 
-// 步骤1：获取code
+// 步骤1：获取puk
+const getPuk = async () => {
+  const { data } = await Taro.request({
+    url: ROUTES.BASE_URL + ROUTES.LOGIN,
+    method: 'GET',
+  })
+  return data // puk
+}
+
+// 步骤2：获取code
 const getWxCode = async (): Promise<string> => {
   const { code } = await Taro.login()
   return code
 }
 
-// 步骤2：检查授权状态
+// 步骤3：检查授权状态
 const checkAuth = async (): Promise<boolean> => {
   const { authSetting } = await Taro.getSetting()
   return !!authSetting['scope.userInfo']
 }
 
-// 步骤3：发送到开发者服务器
+// 步骤4：发送到开发者服务器
 const sendToServer = async (code: string) => {
   const { data } = await Taro.request({
-    url: `${ROUTES.BASE_URL}/login`,
+    url: ROUTES.BASE_URL + ROUTES.LOGIN,
     method: 'POST',
     data: { code },
   })
@@ -28,27 +37,32 @@ const sendToServer = async (code: string) => {
 
 // 完整登录流程
 export const login = async (): Promise<void> => {
-  // 1. 检查本地token
+  // 1. 获取puk，并存储
+  const resPuk = Taro.getStorageSync(STORAGE_KEY.PUK) || (await getPuk())
+  if (!resPuk) return
+  Taro.setStorageSync(STORAGE_KEY.PUK, resPuk.data)
+
+  // 2. 检查本地token
   const token = Taro.getStorageSync(STORAGE_KEY.TOKEN)
   if (token) return
 
-  // 2. 获取微信code
+  // 3. 获取微信code
   const code = await getWxCode()
 
-  // 3. 检查授权状态
+  // 4. 检查授权状态
   const isAuthed = await checkAuth()
   if (!isAuthed) {
     console.warn(new Error('需要用户主动授权'))
     return
   }
 
-  // 4. 发送到服务端
-  const serverData = await sendToServer(code)
-  if (!serverData) return
-  console.log(serverData)
+  // 5. 发送到服务端
+  const resToken = await sendToServer(code)
+  if (!resToken) return
+  console.log(resToken)
 
-  // 5. 存储登录态
-  Taro.setStorageSync(STORAGE_KEY.TOKEN, serverData.token)
+  // 6. 存储登录态
+  Taro.setStorageSync(STORAGE_KEY.TOKEN, resToken.token)
 
   return
 }
